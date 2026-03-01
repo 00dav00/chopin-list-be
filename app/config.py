b505dfa11751a15fb1baf821fba5b69bfabcf045
David Revelo <d.revelo@gresb.com>
2026-03-01 08:51:53 +0100

    Clean up settings

--- app/config.py
+++ app/config.py
@@ -4,20 +4,16 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     mongo_uri: str = Field(
-        validation_alias="MONGO_URI",
+        alias="MONGO_URI",
     )
     mongo_db: str = Field(
-        validation_alias="MONGO_DB",
+        alias="MONGO_DB",
     )
-    google_client_id: str
+    google_client_id: str = Field(
+        alias="GOOGLE_CLIENT_ID",
+    )
     chopin_list_fe_url: str = Field(
-        validation_alias="CHOPIN_LIST_FE_URL",
-    )
-
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
-        extra="ignore",
+        alias="CHOPIN_LIST_FE_URL",
     )
 
 
